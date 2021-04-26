2a6b4b926496d60748d963c444e019f89a629a82
Rodi Scheele <rooddei@hotmail.com>
2021-04-26 10:23:23 +0200

    Initial code

---
+++ bot.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+import discord
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+if os.getenv('DISCORD_TOKEN') != 0:
+    TOKEN = os.getenv('DISCORD_TOKEN')
+else:
+    sys.exit('No environment token found, please configure this first.')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
