ffaa9e8448d9049b2de0d4542c75322db64b2a1d
Rodi Scheele <rooddei@hotmail.com>
2021-04-27 14:43:26 +0200

    Cleaned up ping file

--- bot/commands/ping.py
+++ bot/commands/ping.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        #self._last_member = None
 
     @commands.command(name='ping')
     async def ping(self, context):
