e010122f26c7fe9a12d042d050f1329862dd7ea7
Rodi Scheele <rooddei@hotmail.com>
2021-04-27 16:16:33 +0200

    Added 'back to' to ping pong

--- bot/commands/ping.py
+++ bot/commands/ping.py
@@ -1,19 +1,31 @@
 from discord.ext import commands
+import discord
 
 
 class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._last_member = None
 
     @commands.command(name='ping', description='Ping! :)')
     async def ping(self, context):
         """Do a ping!"""
-        await context.send('Pong!')
+        member = context.author
+        if self._last_member is not None:
+            await context.send("Pong! Back to <@" + str(self._last_member.id) + ">.")
+        else:
+            await context.send("Pong!")
+        self._last_member = member
 
-    @commands.command(name='pong', description='Ping! (:')
+    @commands.command(name='pong', description='Pong! (:')
     async def pong(self, context):
         """Do a pong!"""
-        await context.send('Ping!')
+        member = context.author
+        if self._last_member is not None:
+            await context.send("Ping! Back to <@" + str(self._last_member.id) + ">.")
+        else:
+            await context.send("Ping!")
+        self._last_member = member
 
 
 def setup(bot):
