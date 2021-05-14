daf8f9defd617f022da78d2f0c0ccb7aa38810ed
Rodi Scheele <rooddei@hotmail.com>
2021-05-14 15:50:47 +0200

    Added check in config to see if running on windows

--- bot/packages/files/config.py
+++ bot/packages/files/config.py
@@ -8,10 +8,10 @@ else:
 PREFIX = "!"
 DESCRIPTION = "I am a humble bot."
 APPLICATION_ID = "833460245455044618"
-# Use this DB string for Linux with the --network="host" command
-# DB_STRING = "mongodb://127.0.0.1:27017/"
-# Use this DB string for windows
-DB_STRING = "mongodb://host.docker.internal:27017/"
+if os.name == 'nt':
+    DB_STRING = "mongodb://host.docker.internal:27017/"
+else:
+    DB_STRING = "mongodb://127.0.0.1:27017/"
 DB_NAME = "simplebot"
 # DB collections
 DB_COLLECTION_DAILY_ROLL_VALUE = "dailyroll_value"
